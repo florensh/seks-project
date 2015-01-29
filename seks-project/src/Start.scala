acd2b1ae7f6c66402cc47a05e1ae772500a6351a
Florens Hückstädt <fhueckst@stud.hs-heilbronn.de>
2015-01-29 14:02:06 +0100

    nach Abgabe

--- seks-project/src/Start.scala
+++ seks-project/src/Start.scala
@@ -1,5 +1,6 @@
 import math.exp
-import scala.collection.immutable.Vector
+import scala.collection.immutable.List
+import scala.annotation.tailrec
 
 object Start {
 
@@ -7,27 +8,42 @@ object Start {
    * Starten des Programms
    */
   def main(args: Array[String]) {
-    val test_network = new Network(Vector(2, 10, 2))
+    val test_network = new Network(List(2, 10, 5, 2, 1))
+    val testInput = List(1.0, 1.0)
 
-    println(test_network.biases)
-    println(test_network.weights)
+    print("created " + test_network.sizes.size + " layer network with ")
+    test_network.sizes.foreach((x: Int) => print(x + " "))
+    println("neurons")
 
-    //    println(test_network.weights.zip(test_network.biases))
-    println(test_network.feedforward(Vector(1, 1)))
+    println("biases are: " + test_network.biases)
+    println("weights are: " + test_network.weights)
+
+    //println("output is: " + test_network.feedforwardId(List(1, 1)))
+    println("---------------------------------------------------------------")
+
+    print("testing the network with ")
+    testInput.foreach((x: Double) => print(x + " "))
+    println("as input")
+    println("output is: " + test_network.feedforwardSigmoid(testInput))
 
   }
 
 }
-/**
- * The list ``sizes`` contains the number of neurons in the
- * respective layers of the network.  For example, if the list
- * was [2, 3, 1] then it would be a three-layer network, with the
- * first layer containing 2 neurons, the second layer 3 neurons,
- * and the third layer 1 neuron.
- */
-class Network(sizes: Vector[Int]) {
+
+class Network(s: List[Int]) {
+
+  val sizes = s
 
   require(!sizes.isEmpty)
+
+  //create random biases for the neurons starting at the second layer
+  val biases = (sizes drop 1) map (s => randomList(s))
+
+  //create random weights for the neurons
+  val weights = ((sizes take sizes.size - 1) zip (sizes drop 1)) map ((s: (Int, Int)) => twoDimRandomList(s))
+
+  def randomList = filledListOfSize(random)(_)
+  def twoDimRandomList = filledTwoDimListOfSize(random)(_)
 
   def random() = scala.util.Random.nextGaussian
   //  def random() = { 1 }
@@ -35,48 +51,54 @@ class Network(sizes: Vector[Int]) {
   /**
    * creates a List with random elements for a given number
    */
-  def randomV[A](i: Int, f: () => A): Vector[A] = {
-    def loop(a: Int, acc: Vector[A]): Vector[A] =
-      if (a == 0) acc
-      else loop(a - 1, acc.:+(f()))
-    loop(i, Vector[A]())
+  def filledListOfSize[A](f: () => A)(s: Int): List[A] = {
+
+    @tailrec
+    def loop(i: Int, acc: List[A]): List[A] =
+      if (i == 0) acc
+      else loop(i - 1, f() :: acc)
+    loop(s, List[A]())
 
   }
 
-  def multi_randomV[A](i: Int, j: Int, f: () => A): Vector[Vector[A]] = {
-    def loop(a: Int, acc: Vector[Vector[A]]): Vector[Vector[A]] =
+  def filledTwoDimListOfSize[A](f: () => A)(x: (Int, Int)): List[List[A]] = {
+
+    @tailrec
+    def loop(a: Int, acc: List[List[A]]): List[List[A]] =
       if (a == 0) acc
-      else loop(a - 1, acc.:+(randomV(i, f)))
-    loop(j, Vector[Vector[A]]())
+      else loop(a - 1, filledListOfSize(f)(x._1) :: acc)
+
+    loop(x._2, List[List[A]]())
+
   }
-
-  //create random biases for the neurons starting at the second layer
-  var biases = sizes.drop(1).map(x => randomV(x, random))
-
-  //create random weights for the neurons
-  var weights = sizes.take(2).zip(sizes.drop(1)).map((x: (Int, Int)) => multi_randomV(x._1, x._2, random))
 
   /**
-   * Return the output of the network if ``a`` is input.
+   * Return the output of the network.
    */
-  def feedforward(a: Vector[Double]): Vector[Double] = {
+  def feedforward(f: Double => Double)(input: List[Double]): List[Double] = {
 
     //loop the layers
-    def loopL(i: Int, accL: Vector[Double]): Vector[Double] = {
+    @tailrec
+    def loopL(i: Int, accL: List[Double]): List[Double] = {
 
       //loop the neurons
-      def loopN(j: Int, l: Int, li: Vector[Double], accN: Vector[Double]): Vector[Double] = {
+      @tailrec
+      def loopN(j: Int, l: Int, li: List[Double], accN: List[Double]): List[Double] = {
         if (j == weights(l - 1).size) accN
-        else loopN(j + 1, l, li, accN.:+(sigmoid((li zip weights(l - 1)(j)).map { Function.tupled(_ * _) }.sum + biases(l - 1)(j))))
+        else loopN(j + 1, l, li, accN :+ f((li zip weights(l - 1)(j)).map { Function.tupled(_ * _) }.sum + biases(l - 1)(j)))
       }
 
       if (i == sizes.size) accL
-      else loopL(i + 1, loopN(0, i, accL, Vector[Double]()))
+      else loopL(i + 1, loopN(0, i, accL, List[Double]()))
 
     }
-    loopL(1, a)
+    if (sizes.head != input.size) throw new IllegalAccessException("input layer and input values must be of same size!")
+    loopL(1, input)
 
   }
+
+  def feedforwardId = feedforward((x: Double) => x)(_)
+  def feedforwardSigmoid = feedforward(sigmoid)(_)
 
   // Miscellaneous functions
   def sigmoid(z: Double): Double = 1.0 / (1.0 + exp(-z))
